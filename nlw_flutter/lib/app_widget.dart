ac6fc3fd6fa131a6064761cac54c50ded3c3a5e2
Gabriel Amorim <amorimdevelopment@gmail.com>
2022-02-16 21:25:13 -0300

    Learning about ML Kit to barcode read.

--- nlw_flutter/lib/app_widget.dart
+++ nlw_flutter/lib/app_widget.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:nlw_flutter/modules/barcode_scanner/barcode_scanner_page.dart';
 import 'package:nlw_flutter/modules/home/home_page.dart';
+import 'package:nlw_flutter/modules/insert_boleto/insert_boleto.dart';
 import 'package:nlw_flutter/modules/splash/splash_page.dart';
 
 import 'modules/login/login_page.dart';
 import 'shared/themes/app_colors.dart';
 
 class AppWidget extends StatelessWidget {
+  AppWidget() {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitDown,
+      DeviceOrientation.portraitUp,
+    ]);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -18,7 +27,8 @@ class AppWidget extends StatelessWidget {
         "/splash": (context) => SplashPage(),
         "/home": (context) => HomePage(),
         "/login": (context) => LoginPage(),
-        "/barcode_scanner": (context) => BarcodeScannerPage()
+        "/barcode_scanner": (context) => BarcodeScannerPage(),
+        "/insert_boleto": (context) => InsertBoletoPage(),
       },
     );
   }
